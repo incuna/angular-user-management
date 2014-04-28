d02fae5fbfd6dcd2a091e7f1726dc80f129c0370
Perry Roper <perryroper@gmail.com>
2014-04-29 00:45:17 +0700

    Default all routes to be anonymous, except profile.

--- registration/scripts/routes.js
+++ registration/scripts/routes.js
@@ -8,29 +8,26 @@
             .when('/register/', {
                 templateUrl: 'templates/registration/register.html',
                 controller: 'RegisterCtrl',
-                anonymous: true,
                 title: gettext('Register')
             })
             .when('/register/verify/:token*\/', {
                 templateUrl: 'templates/registration/verify.html',
-                controller: 'RegisterVerifyCtrl',
-                anonymous: true
+                controller: 'RegisterVerifyCtrl'
             })
             .when('/password-reset/', {
                 templateUrl: 'templates/registration/password_reset_request.html',
                 controller: 'PasswordResetRequestCtrl',
-                anonymous: true,
                 title: 'Reset your password'
             })
             .when('/password-change/:token*\/', {
                 templateUrl: 'templates/registration/password_change.html',
                 controller: 'PasswordChangeCtrl',
-                anonymous: true,
                 title: gettext('Set a new password')
             })
             .when('/profile/', {
                 templateUrl: 'templates/registration/profile.html',
                 controller: 'ProfileCtrl',
+                anonymous: false,
                 title: gettext('My profile')
             });
     }]);
