3d4f4427906dde3044d0cb26b31c57ffdb798ab8
Perry Roper <perryroper@gmail.com>
2014-09-11 17:10:23 +0100

    Refactor registration module.

---
+++ src/registration/scripts/init.js
@@ -0,0 +1,10 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('user_management.registration', [
+        'ngRoute',
+        'project_settings'
+    ]);
+
+}(window.angular));
+
