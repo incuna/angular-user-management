efe15ccd8d7d5ec2f85fc30aa1fd24035fbdebb0
Ming Lee <ming@incuna.com>
2016-06-28 14:04:44 +0100

    Fix node file formats and switch off eslint rule: strict.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,162 +1,161 @@
 /* eslint camelcase: off */
-(function () {
-    'use strict';
+/* eslint strict: off */
 
-    var fs = require('fs');
-    var _ = require('lodash');
+'use strict';
 
-    module.exports = function (grunt) {
+var fs = require('fs');
+var _ = require('lodash');
 
-        grunt.initConfig({});
+module.exports = function (grunt) {
 
-        require('time-grunt')(grunt);
+    grunt.initConfig({});
 
-        if (grunt.option('help')) {
-            require('load-grunt-tasks')(grunt);
-        } else {
-            require('jit-grunt')(grunt, {
-                ngtemplates: 'grunt-angular-templates'
-            });
-        }
+    require('time-grunt')(grunt);
 
-        // Get a list of modules
-        var modules = fs.readdirSync('src').filter(function (file) {
-            return fs.statSync('src/' + file).isDirectory();
+    if (grunt.option('help')) {
+        require('load-grunt-tasks')(grunt);
+    } else {
+        require('jit-grunt')(grunt, {
+            ngtemplates: 'grunt-angular-templates'
         });
-        var concatConfig = {};
-        var ngtemplatesConfig = {};
-        var uglifyConfig = {};
-        _.each(modules, function (module) {
-            var modulePath = 'src/' + module;
-            concatConfig[module] = {
-                src: [
-                    modulePath + '/scripts/init.js',
-                    modulePath + '/scripts/providers.js',
-                    modulePath + '/scripts/routes.js',
-                    modulePath + '/scripts/services.js',
-                    modulePath + '/scripts/controllers.js',
-                    modulePath + '/scripts/directives.js'
+    }
+
+    // Get a list of modules
+    var modules = fs.readdirSync('src').filter(function (file) {
+        return fs.statSync('src/' + file).isDirectory();
+    });
+    var concatConfig = {};
+    var ngtemplatesConfig = {};
+    var uglifyConfig = {};
+    _.each(modules, function (module) {
+        var modulePath = 'src/' + module;
+        concatConfig[module] = {
+            src: [
+                modulePath + '/scripts/init.js',
+                modulePath + '/scripts/providers.js',
+                modulePath + '/scripts/routes.js',
+                modulePath + '/scripts/services.js',
+                modulePath + '/scripts/controllers.js',
+                modulePath + '/scripts/directives.js'
+            ],
+            dest: 'dist/' + module + '/' + module + '.js'
+        };
+
+        ngtemplatesConfig[module] = {
+            cwd: modulePath,
+            src: 'templates/user_management/**/*.html',
+            dest: 'dist/' + module + '/templates.js',
+            options: {
+                module: 'user_management.' + module
+            }
+        };
+
+        uglifyConfig[module] = {
+            files: [{
+                expand: true,
+                cwd: 'dist/' + module,
+                src: '**/*.js',
+                dest: 'dist/' + module
+            }]
+        };
+    });
+
+    grunt.config.merge({
+        // Configurable paths
+        config: {
+            modules: 'src',
+            dist: 'dist',
+            lib: 'bower_components',
+            tests: 'tests',
+            files: {
+                lint: [
+                    'src/**/*.js',
+                    '<%= config.files.karmaMocks %>',
+                    '<%= config.files.karmaTests %>',
+                    './grunt/**/*.js',
+                    'Gruntfile.js'
                 ],
-                dest: 'dist/' + module + '/' + module + '.js'
-            };
-
-            ngtemplatesConfig[module] = {
-                cwd: modulePath,
-                src: 'templates/user_management/**/*.html',
-                dest: 'dist/' + module + '/templates.js',
-                options: {
-                    module: 'user_management.' + module
-                }
-            };
-
-            uglifyConfig[module] = {
-                files: [{
-                    expand: true,
-                    cwd: 'dist/' + module,
-                    src: '**/*.js',
-                    dest: 'dist/' + module
-                }]
-            };
-        });
-
-        grunt.config.merge({
-            // Configurable paths
-            config: {
-                modules: 'src',
-                dist: 'dist',
-                lib: 'bower_components',
-                tests: 'tests',
-                files: {
-                    lint: [
-                        'src/**/*.js',
-                        '<%= config.files.karmaMocks %>',
-                        '<%= config.files.karmaTests %>',
-                        './grunt/**/*.js',
-                        'Gruntfile.js'
-                    ],
-                    karmaHelpers: '<%= config.tests %>/helpers/**/*.js',
-                    karmaMocks: '<%= config.tests %>/mocks/**/*.js',
-                    karmaTests: '<%= config.tests %>/unit/**/*.js',
-                    distScripts: '<%= config.dist %>/**/*.js',
-                    distScriptsNoTemplates: '<%= config.dist %>/**/!(templates).js'
-                },
-                htmlmin: {
-                    collapseBooleanAttributes: true,
-                    collapseWhitespace: true,
-                    removeAttributeQuotes: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    removeScriptTypeAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    useShortDoctype: true
-                }
+                karmaHelpers: '<%= config.tests %>/helpers/**/*.js',
+                karmaMocks: '<%= config.tests %>/mocks/**/*.js',
+                karmaTests: '<%= config.tests %>/unit/**/*.js',
+                distScripts: '<%= config.dist %>/**/*.js',
+                distScriptsNoTemplates: '<%= config.dist %>/**/!(templates).js'
             },
-            eslint: {
-                all: {
-                    options: {
-                        config: '.eslintrc'
-                    },
-                    src: '<%= config.files.lint %>'
-                }
-            },
-            jscs: {
+            htmlmin: {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        },
+        eslint: {
+            all: {
                 options: {
-                    config: '.jscsrc'
+                    config: '.eslintrc'
                 },
                 src: '<%= config.files.lint %>'
+            }
+        },
+        jscs: {
+            options: {
+                config: '.jscsrc'
             },
-            watch: {
-                templates: {
-                    files: 'src/**/templates/**/*.html',
-                    tasks: 'ngtemplates'
-                },
-                js: {
-                    files: 'src/**/scripts/**/*.js',
-                    tasks: 'dist'
-                }
+            src: '<%= config.files.lint %>'
+        },
+        watch: {
+            templates: {
+                files: 'src/**/templates/**/*.html',
+                tasks: 'ngtemplates'
             },
-            ngtemplates: _.extend({
-                options: {
-                    htmlmin: '<%= config.htmlmin %>'
+            js: {
+                files: 'src/**/scripts/**/*.js',
+                tasks: 'dist'
+            }
+        },
+        ngtemplates: _.extend({
+            options: {
+                htmlmin: '<%= config.htmlmin %>'
+            }
+        }, ngtemplatesConfig),
+        concat: _.extend({}, concatConfig),
+        uglify: _.extend({
+            options: {
+                compress: {
+                    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+                    drop_console: true
+                    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
                 }
-            }, ngtemplatesConfig),
-            concat: _.extend({}, concatConfig),
-            uglify: _.extend({
-                options: {
-                    compress: {
-                        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-                        drop_console: true
-                        // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
-                    }
-                }
-            }, uglifyConfig)
-        });
+            }
+        }, uglifyConfig)
+    });
 
-        grunt.loadTasks('./grunt');
+    grunt.loadTasks('./grunt');
 
-        grunt.registerTask('dist', [
-            'concat',
-            'uglify',
-            'ngtemplates'
-        ]);
+    grunt.registerTask('dist', [
+        'concat',
+        'uglify',
+        'ngtemplates'
+    ]);
 
-        grunt.registerTask('default', [
-            'dist',
-            'watch'
-        ]);
+    grunt.registerTask('default', [
+        'dist',
+        'watch'
+    ]);
 
-        grunt.registerTask('lint', 'Run the JS linters.', [
-            'eslint',
-            'jscs'
-        ]);
+    grunt.registerTask('lint', 'Run the JS linters.', [
+        'eslint',
+        'jscs'
+    ]);
 
-        grunt.registerTask('test', [
-            'lint',
-            'dist',
-            'test-module-isolation',
-            'karma:ci'
-        ]);
+    grunt.registerTask('test', [
+        'lint',
+        'dist',
+        'test-module-isolation',
+        'karma:ci'
+    ]);
 
-    };
-
-}());
+};
--- grunt/karma.js
+++ grunt/karma.js
@@ -1,115 +1,114 @@
-(function () {
-    'use strict';
+/* eslint strict: off */
 
-    var _ = require('lodash');
+'use strict';
 
-    module.exports = function (grunt) {
+var _ = require('lodash');
 
-        var moduleNames = [
-            'password',
-            'profile',
-            'registration',
-            'sms-password',
-            'sms-verification',
-            'verification'
-        ];
+module.exports = function (grunt) {
 
-        var setupFiles = [
-            '<%= config.lib %>/angular/angular.js',
-            '<%= config.lib %>/angular-mocks/angular-mocks.js',
-            '<%= config.lib %>/angular-route/angular-route.js',
-            '<%= config.lib %>/angular-bootstrap/ui-bootstrap-tpls.js',
+    var moduleNames = [
+        'password',
+        'profile',
+        'registration',
+        'sms-password',
+        'sms-verification',
+        'verification'
+    ];
 
-            '<%= config.files.karmaHelpers %>',
-            '<%= config.files.karmaMocks %>'
-        ];
-        var allTestFiles = [
-            '<%= config.files.karmaTests %>'
-        ];
+    var setupFiles = [
+        '<%= config.lib %>/angular/angular.js',
+        '<%= config.lib %>/angular-mocks/angular-mocks.js',
+        '<%= config.lib %>/angular-route/angular-route.js',
+        '<%= config.lib %>/angular-bootstrap/ui-bootstrap-tpls.js',
 
-        var makeGruntKarmaFiles = function (moduleFiles, specificTestFiles) {
-            var arrayOfSrcFiles = [].concat(setupFiles, moduleFiles, specificTestFiles || allTestFiles);
-            return [
-                {
-                    src: arrayOfSrcFiles
-                }
-            ];
-        };
+        '<%= config.files.karmaHelpers %>',
+        '<%= config.files.karmaMocks %>'
+    ];
+    var allTestFiles = [
+        '<%= config.files.karmaTests %>'
+    ];
 
-        var ciOptions = {
-            singleRun: true,
-            reporters: ['dots', 'coverage'],
-            browsers: ['Firefox'],
-            logLevel: 'DEBUG'
-        };
-
-        var allModuleFiles = [];
-        var individualModuleTargets = {};
-        moduleNames.forEach(function (moduleName) {
-            var individualModuleFiles = [
-                '<%= config.dist %>/' + moduleName + '/' + moduleName + '.js',
-                '<%= config.dist %>/' + moduleName + '/templates.js'
-            ];
-            var individualTestFiles = [
-                '<%= config.tests %>/unit/' + moduleName + '/**/*.js'
-            ];
-            allModuleFiles = allModuleFiles.concat(individualModuleFiles);
-            individualModuleTargets[moduleName] = {
-                options: ciOptions,
-                files: makeGruntKarmaFiles(individualModuleFiles, individualTestFiles)
-            };
-        });
-
-        var allKarmaFiles = makeGruntKarmaFiles(allModuleFiles);
-
-        grunt.config.merge({
-            karma: {
-                options: {
-                    basePath: '',
-                    exclude: [
-                    ],
-                    frameworks: ['jasmine'],
-                    plugins: [
-                       'karma-jasmine',
-                       'karma-firefox-launcher',
-                       'karma-chrome-launcher',
-                       'karma-coverage'
-                    ],
-                    preprocessors: {
-                        '<%= config.files.distScriptsNoTemplates %>': 'coverage'
-                    },
-                    reporters: ['progress', 'coverage'],
-                    port: 9876,
-                    colors: true,
-                    browsers: ['Chrome', 'Firefox'],
-                    logLevel: 'INFO'
-                },
-                dev: {
-                    options: {
-                        browsers: ['Chrome'],
-                        autoWatch: true
-                    },
-                    files: allKarmaFiles
-                },
-                ci: {
-                    options: ciOptions,
-                    files: allKarmaFiles
-                }
+    var makeGruntKarmaFiles = function (moduleFiles, specificTestFiles) {
+        var arrayOfSrcFiles = [].concat(setupFiles, moduleFiles, specificTestFiles || allTestFiles);
+        return [
+            {
+                src: arrayOfSrcFiles
             }
-        });
-
-        grunt.config.merge({
-            karma: individualModuleTargets
-        });
-
-        grunt.registerTask('test-module-isolation', function () {
-            var targets = _.keys(individualModuleTargets);
-            var tasks = _.map(targets, function (target) {
-                return 'karma:' + target;
-            });
-            grunt.task.run(tasks);
-        });
-
+        ];
     };
 
-}());
+    var ciOptions = {
+        singleRun: true,
+        reporters: ['dots', 'coverage'],
+        browsers: ['Firefox'],
+        logLevel: 'DEBUG'
+    };
+
+    var allModuleFiles = [];
+    var individualModuleTargets = {};
+    moduleNames.forEach(function (moduleName) {
+        var individualModuleFiles = [
+            '<%= config.dist %>/' + moduleName + '/' + moduleName + '.js',
+            '<%= config.dist %>/' + moduleName + '/templates.js'
+        ];
+        var individualTestFiles = [
+            '<%= config.tests %>/unit/' + moduleName + '/**/*.js'
+        ];
+        allModuleFiles = allModuleFiles.concat(individualModuleFiles);
+        individualModuleTargets[moduleName] = {
+            options: ciOptions,
+            files: makeGruntKarmaFiles(individualModuleFiles, individualTestFiles)
+        };
+    });
+
+    var allKarmaFiles = makeGruntKarmaFiles(allModuleFiles);
+
+    grunt.config.merge({
+        karma: {
+            options: {
+                basePath: '',
+                exclude: [
+                ],
+                frameworks: ['jasmine'],
+                plugins: [
+                   'karma-jasmine',
+                   'karma-firefox-launcher',
+                   'karma-chrome-launcher',
+                   'karma-coverage'
+                ],
+                preprocessors: {
+                    '<%= config.files.distScriptsNoTemplates %>': 'coverage'
+                },
+                reporters: ['progress', 'coverage'],
+                port: 9876,
+                colors: true,
+                browsers: ['Chrome', 'Firefox'],
+                logLevel: 'INFO'
+            },
+            dev: {
+                options: {
+                    browsers: ['Chrome'],
+                    autoWatch: true
+                },
+                files: allKarmaFiles
+            },
+            ci: {
+                options: ciOptions,
+                files: allKarmaFiles
+            }
+        }
+    });
+
+    grunt.config.merge({
+        karma: individualModuleTargets
+    });
+
+    grunt.registerTask('test-module-isolation', function () {
+        var targets = _.keys(individualModuleTargets);
+        var tasks = _.map(targets, function (target) {
+            return 'karma:' + target;
+        });
+        grunt.task.run(tasks);
+    });
+
+};
